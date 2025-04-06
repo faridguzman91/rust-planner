2dbe15fecf99ae55f3046dfa8a9d7612326f2464
faridguzman91 <farid.guzman91@hotmail.com>
2025-04-06 22:34:18 +0200

    added structs to error res, status codes

--- src/api/task.rs
+++ src/api/task.rs
@@ -18,6 +18,43 @@ pub struct TaskIndentifier {
 
 }
 
+#[derive(Deserialize)]
+pub struct TaskCompletionRequest {
+    result_file: String
+}
+
+#[derive(Deserialize)]
+pub struct SubmitTaskRequest {
+    user_id: String,
+    task_type: String,
+    source_file: String
+}
+
+#[derive(Debug, Display)]
+pub enum TaskError {
+    TaskNotFound,
+    TaskUpdateFailure,
+    TaskCreationFailure,
+    BadTaskRequest
+}
+
+impl ResponseError for TaskError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+        .insert_header(ContentType::json())
+        .body(self.to_string())
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match self {
+            TaskError::TaskNotFound => StatusCode::NOT_FOUND,
+            TaskError::TaskUpdateFailure => StatusCode::FAILED_DEPENDENCY,
+            TaskError::TaskCreationFailure => StatusCode::FAILED_DEPENDENCY,
+            TaskError::BadTaskRequest => StatusCode::BAD_REQUEST
+        }
+        
+    }
+}
 
 #[get("/task/{task_global_id}")]
 pub async fn get_task(task_identifier: Path<TaskIndentifier>, body: Json<Struct>) -> Json<String>{
