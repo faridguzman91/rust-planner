e435425462f06f6d48ecd2ffc9e649c5e9e4758b
faridguzman91 <farid.guzman91@hotmail.com>
2025-04-06 21:49:33 +0200

    implemented get_task fn

--- src/repository/ddb.rs
+++ src/repository/ddb.rs
@@ -40,7 +40,7 @@ fn item_value(
 fn item_to_task(item: &HashMap<String, AttributeValue>) -> Result<Task, DDBError> {
     let state: TaskState = match TaskState::from_str(required_item_value("state", item)?.as_str()) {
         Ok(value) => value,
-        Err(_) => return Err(DDBError)
+        Err(_) => return Err(DDBError),
     };
 
     let result_file = item_value("result_file", item)?;
@@ -51,6 +51,74 @@ fn item_to_task(item: &HashMap<String, AttributeValue>) -> Result<Task, DDBError
         task_type: required_item_value("task_type")?,
         state,
         source_file: required_item_value("source_file", item)?,
-        result_file
+        result_file,
     })
+}
+
+impl DDBRepository {
+    pub fn init(table_name: String, config: Config) -> DDBRepository {
+        let client = Client::new(&config);
+        DDBRepository { table_name, client }
+    }
+
+    pub async fn put_task(&self, task: Task) -> Result<(), DDBError> {
+        dbg!(&task);
+        let mut request = self
+            .client
+            .put_item()
+            .table_name(&self.table_name)
+            .item("pK", AttributeValue::S(String::from(task.user_uuid)))
+            .item("sK", AttributeValue::S(String::from(task.task_uuid)))
+            .item("task_type", AttributeValue::S(String::from(task.task_type)))
+            .item("state", AttributeValue::S(task.state.to_string()))
+            .item(
+                "source_file",
+                AttributeValue::S(String::from(task.source_file)),
+            );
+
+        if let Some(result_file) = task.result_file {
+            request = request.item("result_file", AttributeValue::S(String::from(result_file)));
+        }
+
+        match request.send().await {
+            Ok(_) => Ok(()),
+            Err(_) => Err(DDBError),
+        }
+    }
+
+    pub async fn get_task(&self, task_id: String) -> Option<Task> {
+        let tokens: Vec<String> = task_id.split("_").map(|x| String::from(x)).collect();
+        let user_uuid = AttributeValue::S(tokens[0].clone());
+        let task_uuid = AttributeValue::S(tokens[1].clone());
+
+        let res = self
+            .client
+            .query()
+            .table_name(&self.table_name)
+            .key_condition_expression("#pk = :user_id and #sK = :task_uuid")
+            .expression_attibute_names("#pK", "pk")
+            .expression_attibute_names("sK", "sK")
+            .expression_attibute_values(":user_id", user_uuid)
+            .expression_attibute_values(":task_uuid", task_uuid)
+            .send()
+            .await;
+
+        return match res {
+            Ok(output) => match output.items {
+                Some(items) => {
+                    let item = &items.first()?;
+                    error!("{:?}", &item);
+                    match item_to_task(item) {
+                        Ok(task) => Some(task),
+                        Err(_) => None,
+                    }
+                }
+                None => None,
+            },
+            Err(error) => {
+                error!("{:?}", error);
+                None
+            }
+        };
+    }
 }
